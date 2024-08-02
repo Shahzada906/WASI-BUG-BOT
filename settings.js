3f94e6c4ce5e8a4e4d97ec65b6d3cd8db782b276
Shahzada906 <shahzadab117@gmail.com>
2024-08-02 23:54:42 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923378209976"
+global.ownername = "🐛 MR ՏᎻᎪᎻᏃᎪᎠᎪ "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
